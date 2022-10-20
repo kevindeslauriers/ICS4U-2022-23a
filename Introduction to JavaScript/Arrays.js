aa6b832f55b0abc1d8bd95e4ebb825968568ea91
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-10-20 07:25:32 -0400

    Intro to JS

---
+++ Introduction to JavaScript/Arrays.js
@@ -0,0 +1,26 @@
+// join
+
+// push
+
+// pop
+
+// shift
+
+// unshift
+
+// concat
+
+// slice
+let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let citrus = fruits.slice(3);
+
+fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+citrus = fruits.slice(1, 3);
+
+
+// splice
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 2, "Lemon", "Kiwi");
