e4c615871ff8427b566f98635ce5039842bf9367
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-10-28 08:27:55 -0400

    V2 Calc

--- Introduction to JavaScript/Arrays.js
+++ Introduction to JavaScript/Arrays.js
@@ -1,26 +1,54 @@
+let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+
 // join
+// console.log(fruits.join());
+// console.log(fruits.join('--'));
 
 // push
+// fruits.push('Kiwi');                         // push a string on to the array
+// console.log(fruits.join());
+// fruits.push(['Strawberry', 'Blueberry']);    // push an array on to the array
+// console.log(fruits.join());
+// fruits.push(5);
+// console.log(fruits.join());
+
+//console.log(JSON.stringify(fruits));
+
 
 // pop
-
+//console.log(fruits.pop());
+//console.log(fruits.join());
 // shift
+// console.log(fruits.join());
+// console.log(fruits.shift());
+// console.log(fruits.join());
+// // unshift
+// console.log(fruits.unshift('Potato'));
+// console.log(fruits.join());
 
-// unshift
+
 
 // concat
+// fruits = fruits.concat(['Strawberry', 'Blueberry']);
+// console.log(JSON.stringify(fruits));
 
 // slice
-let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-let citrus = fruits.slice(3);
-
 fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-citrus = fruits.slice(1, 3);
+let citrus = fruits.slice(3);
+// console.log(citrus);
+// console.log(fruits);
 
+
+// fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// citrus = fruits.slice(1, 3);
+// console.log(citrus);    // returned ["Orange", "Lemon"]
+// console.log(fruits);    ["Banana", "Orange", "Lemon", "Apple", "Mango"]
 
 // splice
 fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.splice(2, 0, "Lemon", "Kiwi");
+fruits.splice(2, 0, "Lemon", "Kiwi");     // splice(where, # to remove, insert these)
+console.log(fruits);
 
-fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.splice(2, 2, "Lemon", "Kiwi");
+fruits = ["Banana", "Orange", "Apple", "Mango", "Pineapple", "Cherry"];
+fruits.splice(2, 1, "Lemon", "Kiwi");
+console.log(fruits);
--- Introduction to JavaScript/HigherOrderfunctions.js
+++ Introduction to JavaScript/HigherOrderfunctions.js
@@ -1,3 +1,44 @@
+let sum = function (a, b) {
+   let result = 0;
+   for (let i = a; i <= b; i++) {
+      result += i;
+   }
+
+   return result;
+}
+
+let sum2 = (a, b) => {
+   let result = 0;
+   for (let i = a; i <= b; i++) {
+      result += i;
+   }
+
+   return result;
+}
+
+// console.log(sum(1, 10));
+// console.log(sum2(1, 10));
+
+
+
+
+// function mystery() {
+//    return 42;
+// }
+
+// function mystery2(callback) {
+//    let result = callback() + 5;
+//    console.log(result);
+// }
+
+// mystery2(mystery);
+
+
+
+
+
+
+
 const companies = [
    { name: "Company One", category: "Finance", start: 1981, end: 2004 },
    { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
@@ -13,14 +54,25 @@ const companies = [
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // forEach
+companies.forEach(val => { console.log(val.name) });     // no () required if 1 arg
+companies.forEach((val, i) => { console.log(`${i}: ${val.name}`) });
+
+
 
 
 
 // filter
 
 // Get 21 and older
+const oldPeople = ages.filter((age) => age >= 21);
+console.log(oldPeople);
+
+
 
 // Filter retail companies
+const retailCompanies = companies.filter(company => company.category === 'Retail');
+console.log(retailCompanies)
+
 
 // Get 80s companies
 
