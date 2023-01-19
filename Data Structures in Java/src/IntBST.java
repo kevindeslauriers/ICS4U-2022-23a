1af92fc5e61e6a41926257d27c0024cef2de5b72
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-01-19 10:39:13 -0500

    Added Delete BST using min on right subtree

--- Data Structures in Java/src/IntBST.java
+++ Data Structures in Java/src/IntBST.java
@@ -20,6 +20,46 @@ public class IntBST {
       preOrderPrintTraversal(root);
    }
 
+   private Integer findSmallest(IntBSTNode root) {
+      Integer min = root.getValue();
+
+      while (root.getLeftChild() != null) {
+         min = root.getLeftChild().getValue();
+         root = root.getLeftChild();
+      }
+
+      return min;
+   }
+
+   public void remove(Integer val) {
+      // removing the root node of our BST
+      root = removeRecursive(root, val);
+
+   }
+
+   private IntBSTNode removeRecursive(IntBSTNode root, Integer val) {
+      if (root == null)
+         return root;
+
+      if (val < root.getValue()) {
+         root.setLeftChild(removeRecursive(root.getLeftChild(), val));
+      } else if (val > root.getValue()) {
+         root.setRightChild(removeRecursive(root.getRightChild(), val));
+      } else {
+         if (root.getLeftChild() == null) {
+            return root.getRightChild();
+         } else if (root.getRightChild() == null) {
+            return root.getLeftChild();
+         } else {
+            Integer biggest = findSmallest(root.getRightChild());
+            root.setValue(biggest);
+            root.setRightChild(removeRecursive(root.getRightChild(), root.getValue()));
+         }
+      }
+
+      return root;
+   }
+
    private void preOrderPrintTraversal(IntBSTNode root) {
       System.out.println(root.getValue());
       if (root.hasLeftChild()) {
