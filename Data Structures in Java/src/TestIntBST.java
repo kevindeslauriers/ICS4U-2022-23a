89cf553b2bbea6d65ab68e7f42308787cd402674
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-01-30 15:04:51 -0500

    Introduction to Vue3

--- Data Structures in Java/src/TestIntBST.java
+++ Data Structures in Java/src/TestIntBST.java
@@ -2,14 +2,11 @@ public class TestIntBST {
    public static void main(String[] args) {
       IntBST bst = new IntBST();
 
-      bst.add(6);
-      bst.add(8);
-      bst.add(3);
-      bst.add(1);
-      bst.add(13);
-      bst.add(9);
       bst.add(7);
-      bst.add(11);
+      bst.add(5);
+      bst.add(8);
+      bst.inOrderPrintTraversal();
+      bst.remove(7);
 
       bst.inOrderPrintTraversal();
 
